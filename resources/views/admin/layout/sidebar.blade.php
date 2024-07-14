39c0cf1025cf6a347dd1554bdac75a301bb2d9c4
AlfadliRPutra <alfadlipoetra@gmail.com>
2024-07-14 09:52:05 +0700

    Menambah Halaman peserta magang

---
+++ resources/views/admin/layout/sidebar.blade.php
@@ -0,0 +1,103 @@
+<div class="sidebar" data-background-color="light">
+    <div class="sidebar-logo">
+      <!-- Logo Header -->
+      <div class="logo-header" data-background-color="grey">
+        <a href="index.html" class="logo">
+          <img src="/img/Telkom.png" alt="navbar brand" class="navbar-brand" height="40" />
+        </a>
+  
+        <div class="nav-toggle">
+          <button class="btn btn-toggle toggle-sidebar">
+            <i class="gg-menu-right"></i>
+          </button>
+          <button class="btn btn-toggle sidenav-toggler">
+            <i class="gg-menu-left-dark"></i>
+          </button>
+        </div>
+        <button class="topbar-toggler more">
+          <i class="gg-more-vertical-alt"></i>
+        </button>
+      </div>
+      <!-- End Logo Header -->
+    </div>
+  
+    <div class="sidebar-wrapper scrollbar scrollbar-inner">
+      <div class="sidebar-content">
+        <ul class="nav nav-secondary">
+          <li class="nav-item active">
+            <a data-bs-toggle="collapse" href="#" class="collapsed" aria-expanded="false">
+              <i class="fas fa-home"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+  
+          <!-- Menu -->
+          <li class="nav-section">
+            <span class="sidebar-mini-icon">
+              <i class="fa fa-ellipsis-h"></i>
+            </span>
+            <h4 class="text-section">Menu</h4>
+          </li>
+          <!-- end menu -->
+  
+          <li class="nav-item active">
+            <a data-bs-toggle="collapse" href="#" class="collapsed" aria-expanded="false">
+              <i class="fas fa-user-friends"></i>
+              <p>Peserta Magang</p>
+            </a>
+          </li>
+  
+          <li class="nav-item">
+            <a data-bs-toggle="collapse" href="#base">
+              <i class="fas fa-layer-group"></i>
+              <p>Presensi</p>
+              <span class="caret"></span>
+            </a>
+            <div class="collapse" id="base">
+              <ul class="nav nav-collapse">
+                <li>
+                  <a href="components/avatars.html">
+                    <span class="sub-item">Hadir</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="components/buttons.html">
+                    <span class="sub-item">Tidak hadir</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item active">
+            <a data-bs-toggle="collapse" href="#" class="collapsed" aria-expanded="false">
+              <i class="fas fa-file-signature"></i>
+              <p>Dokumen</p>
+            </a>
+          </li>
+  
+          <!-- Menu -->
+          <li class="nav-section">
+            <span class="sidebar-mini-icon">
+              <i class="fa fa-ellipsis-h"></i>
+            </span>
+            <h4 class="text-section">Lainnya</h4>
+          </li>
+          <!-- end menu -->
+          <li class="nav-item active">
+            <a data-bs-toggle="collapse" href="#" class="collapsed" aria-expanded="false">
+              <i class="fas fa-cog"></i>
+              <p>Pengaturan</p>
+            </a>
+          </li>
+
+          <li class="nav-item active">
+            <a data-bs-toggle="collapse" href="#" class="collapsed" aria-expanded="false">
+              <i class="fas fa-question-circle"></i>
+              <p>Bantuan</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+});
+
+Route::get('/admin/intern', function () {
+    return view('admin.intern');
+});
